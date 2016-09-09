355fcaf07c91a2a2576450f523a3ffb67d9fc403
Shaun <seyyes2000@hotmail.com>
2016-09-09 07:15:16 -0400

    update primeFactor.js

--- primeFactor.js
+++ primeFactor.js
@@ -1,25 +1,15 @@
 var largestPrimeFactor = function(num) {
-  
-    divisNums = [];
-    for (var i = 2; i <= num; i++) {
-      if (num % i == 0) {
-          divisNums.push(i);
-        }
-    }
 
-    console.log(divisNums);
-    
-    var index;
-    for (index = divisNums.length -1; index >= 0; --index) {
-      for (var j = 2; j < index; j++) {
-          if (index % j == 0) {
-              break;
-            } else {
-              return divisNums[j];
-            }
-        }
+    var i = 2;
+  while (i < num) {
+        if (num % i == 0) {
+            i++;
+            break;
+      } else {
+            i++;
+        return i;
+      }
     }
-    //return divisNums[j];
-};
+}
 
-console.log(largestPrimeFactor(33));
+console.log(largestPrimeFactor(10));
