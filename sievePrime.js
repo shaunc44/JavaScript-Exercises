d1ab54ef19a428e2ed7f84b35153b6e83fef4272
Shaun <seyyes2000@hotmail.com>
2016-09-12 05:50:14 -0400

    finish sievePrime.js

--- sievePrime.js
+++ sievePrime.js
@@ -2,20 +2,24 @@ var sieve = function(num) {
 
     var primeArray = [];
     if (num < 2) {
-        return primeOne;
+        return primeArray;
     }
-    
+
     var i = 2;
     while (i <= num) {
         var j = 2;
-        while (j <= i) {
-            if (num % i == 0) { 
+        while (j < i) {
+            if (i % j == 0) { 
                 break;
-            } 
-            primeArray.push(i);
+            }
+            j++;
+        }
+        if (primeArray.indexOf(j) < 0) {
+            primeArray.push(j);
         }
         i++;
     }
+    return primeArray;
 };
 
-console.log(sieve(10));
+console.log(sieve(19));
